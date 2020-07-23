57e1e68ba9ce0ad448d9f921f8cb06a2283a7b86
Josh Reilly <josh.m.reilly@gmail.com>
2020-07-22 22:21:50 -0500

    chore: progress on JS

--- public/main.js
+++ public/main.js
@@ -6,32 +6,57 @@ var fire_buzzer = fire_buzzer || ((namespace) => {
         console.log(error);
     }
 
+    namespace.refs = {};
+
+    namespace._get_refs = () => {
+        namespace.refs.buzzerRef = firebase.database().ref('buzzer')
+    }
+
+    namespace.addEventListeners = () => {
+        document.getElementById('btnBuzzer').addEventListener('click', namespace.handleClicks.btnBuzzer)
+        document.getElementById('btnReset').addEventListener('click', namespace.handleClicks.btnReset)
+        document.getElementById('btnAdminLogin').addEventListener('click', namespace.handleClicks.btnAdminLogin)
+    }
+
+    namespace.handleClicks = {
+        btnBuzzer: () => namespace.refs.buzzerRef.set(true),
+        btnReset: () => namespace.refs.buzzerRef.set(false),
+        btnAdminLogin: () => {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                console.log(token);
+                console.log(user);
+              }).catch(namespace.handleErrors);
+        }
+    }
+
     namespace.onLoad = () => {
+        let app = firebase.app();
 
-          let app = firebase.app();
+        namespace._get_refs();
+        namespace.addEventListeners();
 
-          firebase.auth().signInAnonymously().then(() => {
+        firebase.auth().signInAnonymously().then(() => {
 
             const uid = firebase.auth().currentUser.uid
 
-            let buzzerRef = firebase.database().ref('buzzer')
+            const buzzerRef = namespace.refs.buzzerRef;
             buzzerRef.on('value', (snapshot) => {
-              console.log(snapshot.val())
-            })
-
-
-            document.getElementById('btnBuzzer').addEventListener('click', () => {
-                buzzerRef.set(true);
-            })
-
-            document.getElementById('btnReset').addEventListener('click', () => {
-              buzzerRef.set(false);
-            })
-
+                const buzzerValue = snapshot.val();
+                console.log(buzzerValue);
+                // if (buzzerValue) {
+                //     document.getElementById('btnBuzzer').style = "";
+                // } else {
+                //     document.getElementById('btnBuzzer').style = "disabled: true";
+                // }
+            });
 
           });
     }
 
     return namespace;
 })({});
-console.log(fire_buzzer);
